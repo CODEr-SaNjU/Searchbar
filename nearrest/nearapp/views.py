7f832d6c2c4f28f920a73a5dcff35eb460d2f0c4
coder-sanju <sanju2help@gmail.com>
2021-06-29 12:02:20 +0530

    distance checkout

--- nearrest/nearapp/views.py
+++ nearrest/nearapp/views.py
@@ -3,16 +3,32 @@ from django.contrib.gis.db.models.functions import Distance
 from .models import *
 from django.contrib.gis.geos import Point
 from .forms import RestaurantRegForm
-longitude = 76.61727904206835
-latitude = 27.23662089731647
+from ip2geotools.databases.noncommercial import DbIpCity
+import socket
+hostname = socket.gethostname()
+ipaddr = socket.gethostbyname(hostname)
+
+
+r = DbIpCity.get('157.38.4.207', api_key='free')
+
+
+longitude = r.longitude
+latitude = r.latitude
+print(longitude, latitude)
 user_location = Point(longitude, latitude, srid=4326)
 
 
 def home(request):
+    user_city = r.city
     queryset = RestaurantReg.objects.annotate(distance=Distance('restaurant_location',
-                                                                user_location)
+                                                                user_location)/1000
                                               ).order_by('distance')[0:6]
-    return render(request, 'frontend/base.htm',  {'queryset': queryset})
+
+    for query in queryset:
+        dist = query.distance
+        if dist <= 861:
+            print(query)
+    return render(request, 'frontend/base.htm',  {'queryset': queryset, 'user_city': user_city})
 
 
 def addrestaurant(request):
